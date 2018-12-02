934248e94344172b60956363df19339d59e4767a
Markus-PC\Markus <markus.brand@student.hpi.de>
2018-12-02 23:48:33 +0100

    Controls

---
+++ Assets/Controls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controls : MonoBehaviour
+{
+    private bool wPressed;
+    private bool aPressed;
+    private bool dPressed;
+
+    private float _timeFading;
+
+    // Update is called once per frame
+    void Update()
+    {
+        wPressed = wPressed || Input.GetKey(KeyCode.W); 
+        aPressed = aPressed || Input.GetKey(KeyCode.A); 
+        dPressed = dPressed || Input.GetKey(KeyCode.D); 
+        if (wPressed && aPressed && dPressed)
+        {
+            _timeFading += Time.deltaTime;
+            Color color = new Color(1, 1, 1, 1 - _timeFading);
+            GetComponent<Image>().color = color;
+        }
+    }
+}
